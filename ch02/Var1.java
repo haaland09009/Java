7225f5e7545891414395c03c1ac839a7156f6c39
09009 <87968927+haaland09009@users.noreply.github.com>
2023-02-15 17:08:55 +0900

    Add files via upload

---
+++ ch02/Var1.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class Var1 {
+	public static void main(String[] args) {
+		int i1 = 12;
+		int i2 = 45;
+		System.out.printf("%d + %d = %d\n", i1, i2, i1 + i2);
+		System.out.printf("%d - %d = %d\n", i1, i2, i1 - i2);
+		System.out.printf("%d * %d = %d\n", i1, i2, i1 * i2);
+//		자바에서는 정수끼리 연산은 결과도 정수
+		System.out.printf("%d / %d = %d\n", i1, i2, i1 / i2);
+
+
+		
+	}
+}
