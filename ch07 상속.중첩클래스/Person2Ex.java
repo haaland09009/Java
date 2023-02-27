c23c2e0dcc9549011ae6cc4cf44ec0f7fdd219c5
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-02-27 17:25:14 +0900

    Update Person2Ex.java

--- ch07 상속.중첩클래스/Person2Ex.java
+++ ch07 상속.중첩클래스/Person2Ex.java
@@ -5,14 +5,11 @@ public class Person2Ex {
 		Person2[] person = new Person2[6];
 		person[0] = new Student2("로제", 26, "970201");
 		person[1] = new Student2("보검", 29, "960201");
-		person[2] = new Student2("제니", 27, "자바");
-		person[3] = new Student2("은우", 32, "DataBase");
-		person[4] = new Student2("효리", 38, "화장실 청소");
-		person[5] = new Student2("화사", 26, "운전");
+		person[2] = new Teacher2("제니", 27, "자바");
+		person[3] = new Teacher2("은우", 32, "DataBase");
+		person[4] = new Manager2("효리", 38, "화장실 청소");
+		person[5] = new Manager2("화사", 26, "운전");
 		
-		for(Person2 p : person) {
-			p.print();
-		}
 	}
 
 }
